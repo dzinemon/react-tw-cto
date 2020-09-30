ce596bb321a1faf9ec9878be9e6a88cd56efe1c4
Andrii S <andriisharapatov@gmail.com>
2020-09-30 15:49:47 +0300

    classname

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () =>
   <div className="flex flex-row justify-center">
-    <div class="text-gray-700 text-center px-4 py-2 m-2">
+    <div className="text-gray-700 text-center px-4 py-2 m-2">
       <p className="text-blue-700 text-6xl font-bold">404</p>
       <p className="text-blue-800 font-bold text-2xl">Сторінку не знайдено</p>
       <p>На жаль, сторінка, яку ви шукаєте, не існує.</p>

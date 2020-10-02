e51f460976be8f4e84eefa73bc5eea8c447cb9a3
Andrii S <andriisharapatov@gmail.com>
2020-10-02 23:39:54 +0300

    update images and hover effect

--- src/components/CarCardItem.js
+++ src/components/CarCardItem.js
@@ -5,9 +5,9 @@ function CarCardItem(props) {
   const { image, modelName } = props;
 
   return(
-    <div className="max-w-xs rounded overflow-hidden shadow-lg my-2">
+    <div className="max-w-xs rounded overflow-hidden shadow-lg my-2 hover:shadow-2xl transition transition-shadow duration-500">
       <img className="w-full h-32 object-cover object-center" src={image} alt={modelName}/>
-      <div className="px-4 py-3">
+      <div className="px-3 py-2">
         <div className="font-bold text-base md:text-xl">{modelName}</div>
       </div>
     </div>

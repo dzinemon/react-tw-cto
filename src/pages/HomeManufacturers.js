e5188c95f6329cd076ffbc0d031eb3f94bf2ae38
Andrii S <andriisharapatov@gmail.com>
2020-10-02 23:51:19 +0300

    small change in styling of data update notice

--- src/pages/HomeManufacturers.js
+++ src/pages/HomeManufacturers.js
@@ -24,7 +24,7 @@ function HomeManufacturers() {
           <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
             Виберіть автовиробника
           </h3>
-          <p className="mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto">
+          <p className="max-w-2xl text-sm md:text-base  leading-7 text-gray-500 lg:mx-auto">
             Ми постійно оновлюемо дані
           </p>
         </div>
--- src/pages/ManufacturerSection.js
+++ src/pages/ManufacturerSection.js
@@ -42,7 +42,7 @@ function ManufacturerSection(props) {
           <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
             Виберіть модель {manufacturer}
           </h3>
-          <p className="mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto">
+          <p className="max-w-2xl text-sm md:text-base  leading-7 text-gray-500 lg:mx-auto">
             Ми постійно оновлюемо дані моделей {manufacturer}
           </p>
         </div>

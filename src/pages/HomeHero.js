77bb57eb003e774881590519c4bea2b171109d90
Andrii S <andriisharapatov@gmail.com>
2020-10-06 13:19:55 +0300

    make the hero user friendly

--- src/pages/HomeHero.js
+++ src/pages/HomeHero.js
@@ -33,12 +33,9 @@ function HomeHero() {
         <p className="mt-3 text-base text-gray-700 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
           Розрахуйте вартість володіння автомобілем
         </p>
-        <p className="mt-3 text-base text-gray-700 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-          Система розраховує додаткові витрати, які ви, можливо, не врахували при розгляді вашої наступної покупки автомобіля. 
-        </p>
-        <p className="mt-3 text-base text-gray-700 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">Ці додаткові витрати включають: податки та збори, страхові внески, витрати на паливо, технічне обслуговування, ремонт та інше. Перегляньте витрати на володіння будь-яким авто.</p>
-        <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-          {/* <div className="rounded-md shadow">
+        
+        {/* <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+          <div className="rounded-md shadow">
             <a href="#manufacturers" className="w-full flex items-center justify-center px-6 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue transition duration-150 ease-in-out md:py-3 md:text-lg md:px-8">
               Перелік авто
             </a>
@@ -47,16 +44,24 @@ function HomeHero() {
             <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-blue-700 bg-blue-100 hover:text-blue-600 hover:bg-blue-50 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
               Live demo
             </a>
-          </div> */}
-        </div>
+          </div> 
+        </div>*/}
       </div>
       <div className="lg:w-1/3 w-full ">
-        <h3 className="text-center mt-2 text-xl leading-8 tracking-tight text-gray-700 sm:text-2xl sm:leading-10">
+        {/* <h3 className="text-center mt-2 text-xl leading-8 tracking-tight text-gray-700 sm:text-2xl sm:leading-10">
           Виберіть автовиробника
-        </h3>
+        </h3> */}
         <div className="flex justify-center items-center">
           {manufacturers}
         </div>
+      </div>
+
+      <div className="text-center lg:text-left lg:w-2/3 w-full">
+      <p className="mt-3 text-base text-gray-700 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+          Система розраховує додаткові витрати, які ви, можливо, не врахували при розгляді вашої наступної покупки автомобіля. 
+        </p>
+        <p className="mt-3 text-base text-gray-700 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+          Ці додаткові витрати включають: податки та збори, страхові внески, витрати на паливо, технічне обслуговування, ремонт та інше. Перегляньте витрати на володіння будь-яким авто.</p>
       </div>
     </main>
   )

e46fa8ceafc3f0ab326d43c12e04b79bb17b6d91
Andrii S <andriisharapatov@gmail.com>
2020-10-18 16:54:24 +0300

    make height bigger, font smaller for car

--- src/components/CarCardItem.js
+++ src/components/CarCardItem.js
@@ -5,10 +5,10 @@ function CarCardItem(props) {
   const { image, modelName } = props;
 
   return(
-    <div className="max-w-xs rounded overflow-hidden shadow-lg my-2 hover:shadow-2xl transition transition-shadow duration-500">
-      <img className="w-full h-32 object-cover object-center" src={image} alt={modelName}/>
-      <div className="px-3 py-2">
-        <div className="font-semibold text-sm md:text-xl">{modelName}</div>
+    <div className="max-w-xs rounded overflow-hidden shadow-md md:shadow-lg my-2 hover:shadow-2xl transition transition-shadow duration-500">
+      <img className="w-full h-32 md:h-40 object-cover object-center" src={image} alt={modelName}/>
+      <div className="px-2 py-1">
+        <div className="font-semibold text-sm md:text-lg">{modelName}</div>
       </div>
     </div>
   )
